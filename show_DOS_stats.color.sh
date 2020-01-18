80f625366d67f6e03946ef85055b5d2fafb4b036
sVen Mueller <58518999+sv3n-mu3ll3r@users.noreply.github.com>
2020-01-18 19:33:12 +0100

    Update show_DOS_stats.color.sh
    changed $9 to $10

--- show_DOS_stats.color.sh
+++ show_DOS_stats.color.sh
@@ -4,7 +4,7 @@ tmctl -c dos_stat -s context_name,vector_name,attack_detected,stats_rate,drops_r
 awk '{ \
         if ($1 == "context_name") print "\033[1m"$0"\033[0m";
         # Mitigation
-        else if (($5>0 || $6>0 || $8> 0 || $9>0)) print "\033[01;31m"$0"\033[0m";
+        else if (($5>0 || $6>0 || $8> 0 || $10>0)) print "\033[01;31m"$0"\033[0m";
         else if ($4>0) print "\033[01;32m"$0"\033[0m";
         else if ($7>0) print "\033[01;32m"$0"\033[0m";
         else if ($9>0) print "\033[01;32m"$0"\033[0m";
