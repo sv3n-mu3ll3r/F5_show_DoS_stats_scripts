ea46e1c51539c505fc7a1d336abdb59605b4613f
sVen Mueller <58518999+sv3n-mu3ll3r@users.noreply.github.com>
2020-01-19 16:04:33 +0100

    Update show_DOS_stats.color.sh

--- show_DOS_stats.color.sh
+++ show_DOS_stats.color.sh
@@ -5,7 +5,7 @@ awk '{ \
         if ($1 == "context_name") print "\033[1m"$0"\033[0m";
         # Mitigation
         else if (($5>0 || $6>0 || $8> 0 || $10>0)) print "\033[01;31m"$0"\033[0m";
+        else if ($3>0) print "\033[01;33m"$0"\033[0m";
         else if ($4>0) print "\033[01;32m"$0"\033[0m";
         else if ($7>0) print "\033[01;32m"$0"\033[0m";
-        else if ($9>0) print "\033[01;32m"$0"\033[0m";
-        else if ($3>0) print "\033[01;33m"$0"\033[0m"; }'
+        else if ($9>0) print "\033[01;32m"$0"\033[0m"; }'
